ad15b739bb995ed37547e8ff0f9b468445a3832f
TryniakIgor <94488222+TryniakIgor@users.noreply.github.com>
2023-01-16 13:48:20 +0200

    done

---
+++ src/main/java/com/chat/model/ChatMessage.java
@@ -0,0 +1,17 @@
+package com.chat.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ChatMessage {
+    private String content;
+    private String sender;
+    private MessageType type;
+
+    public enum MessageType {
+        CHAT, LEAVE, JOIN
+    }
+
+}
